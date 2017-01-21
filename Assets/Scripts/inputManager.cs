1a358105f767baba1e244cf85083ec023baa2ed7
kevin-kzc <kevin72lq@gmail.com>
2017-01-21 01:29:10 -0300

    Button joysticks recognized.

---
+++ Assets/Scripts/inputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputManager : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0))
+            print("A button");
+        if (Input.GetKeyDown(KeyCode.Joystick1Button1))
+            print("B button");
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
+            print("X button");
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3))
+            print("Y button");
+        if (Input.GetKeyDown(KeyCode.Joystick1Button7))
+            print("START button");
+    }
+}
